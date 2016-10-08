b37d18c49c87285b73298bcac9bdbbabbdbbde15
joe.wu <traveller_wjoe@sina.com>
2016-10-08 22:34:11 +0800

    change to dynamic generate html

--- js/jquery.SlwyTree.js
+++ js/jquery.SlwyTree.js
@@ -1,9 +1,5 @@
 (function($) {
     $.fn.SlwyTree = function(setting, treeNodes) {
-        var selector = {
-            selectedTreeListEl: $('.slwyTree-selected-list'),
-            selectedTreeCountEl: $('#selectedTreeCount')
-        };
 
         function slwyTree(selector, setting, treeNodes) {
             this.default = {
@@ -23,18 +19,26 @@
                 searchType: 1
             }
             this.selector = selector;
-            this.setting = $.extend(setting, this.default);
+            this.setting = $.extend(this.default, setting);
             this.treeNodes = treeNodes;
             this.searchNodes = []; //搜索匹配节点,默认空
             this.searchFilter = [null, this.searchFilterHighlight, this.searchFilterShow]; //搜索Filter
+            this.selectedNodeList = []; //已选择的节点列表
             this.init();
         }
         slwyTree.prototype = {
             constructor: slwyTree,
             init: function() {
-                this.ztreeObj = $.fn.zTree.init(this.selector, this.setting, this.treeNodes);
-                this.selectedTreeList = [];
+                this.render();
+                this.initSelector();
+                this.ztreeObj = $.fn.zTree.init($('#zTree'), this.setting, this.treeNodes);
                 this.bind();
+            },
+            initSelector: function() {
+                this.selectorEl = {
+                    selectedNodeListEl: $('.slwyTree-selected-list'),
+                    selectedTreeCountEl: $('#selectedTreeCount')
+                };
             },
             bind: function() {
                 if (this.setting.searchable) this.bindSearch();
@@ -49,11 +53,11 @@
                 var _self = this;
                 this.ztreeObj.setting.callback.onCheck = function(e, id, node) {
                     if (node.checked) {
-                        _self.selectedTreeList.push(node);
+                        _self.selectedNodeList.push(node);
                     } else {
-                        _self.selectedTreeList.splice(_self.selectedTreeList.indexOf(node), 1);
+                        _self.selectedNodeList.splice(_self.selectedNodeList.indexOf(node), 1);
                     }
-                    _self.renderSelectedTreeList();
+                    _self.renderSelectedNodeList();
                     _self.updateSelectedTreeCount();
                 }
             },
@@ -66,24 +70,50 @@
                 })
                 $(document).on('click', '.slwyTree-remove-all', function(e) {
                     _self.ztreeObj.checkAllNodes(false);
-                    _self.selectedTreeList = [];
-                    selector.selectedTreeListEl.html('');
-                    selector.selectedTreeCountEl.text(0);
+                    _self.selectedNodeList = [];
+                    this.selectorEl.selectedNodeListEl.html('');
+                    this.selectorEl.selectedTreeCountEl.text(0);
                 })
             },
-            renderSelectedTreeList: function() {
-                var html = '';
-                for (var i = 0; i < this.selectedTreeList.length; i++) {
-                    html += '<li data-tid="' + this.selectedTreeList[i].tId + '">' + this.selectedTreeList[i].name + '<a href="javascript:;" class="iconfont slwyTree-remove fr">&#xe60b;</a></li>';
+            render: function() {
+                var slwyTreeDiv = $('<div class="slwyTree slwyTree-select">'),
+                    zTreeDiv = $('<div class="ztree" id="zTree"></div>'),
+                    searchBoxDiv,
+                    selectBoxDiv;
+                slwyTreeDiv.append(zTreeDiv);
+                if (this.setting.searchable) {
+                    searchBoxDiv = $('<div class="search-box">');
+                    searchBoxDiv.append('<i class="iconfont icon-search">&#xe60c;</i><input type="search" id="zTreeSearch" placeholder="搜索部门">');
+                    zTreeDiv.before(searchBoxDiv);
                 }
-                selector.selectedTreeListEl.html(html);
+                if (this.setting.selectable) {
+                    if (!this.setting.selectBoxEl) {
+                        throw new Error('If you set selectable as true,you should pass parameter "selectBoxEl" as well.');
+                    }
+                    var html = [];
+                    selectBoxDiv = $(this.setting.selectBoxEl);
+                    html.push('<div class="slwyTree slwyTree-select">');
+                    html.push('<div class="slwy-color-active slwyTree-title">已选:<span id="selectedTreeCount">0</span><a href="javascript:;" class="iconfont slwyTree-remove-all fr">&#xe60b;</a></div>');
+                    html.push('<ul class="slwyTree-selected-list"></ul>');
+                    html.push('</div>');
+
+                    selectBoxDiv.append(html.join(''));
+                }
+                this.selector.html(slwyTreeDiv);
+            },
+            renderSelectedNodeList: function() {
+                var html = '';
+                for (var i = 0; i < this.selectedNodeList.length; i++) {
+                    html += '<li data-tid="' + this.selectedNodeList[i].tId + '">' + this.selectedNodeList[i].name + '<a href="javascript:;" class="iconfont slwyTree-remove fr">&#xe60b;</a></li>';
+                }
+                this.selectorEl.selectedNodeListEl.html(html);
             },
             updateSelectedTreeCount: function() {
-                selector.selectedTreeCountEl.text(this.selectedTreeList.length);
+                this.selectorEl.selectedTreeCountEl.text(this.selectedNodeList.length);
             },
             highlightNode: function(highlight) {
                 var _self = this;
-                this.searchNodes.forEach(function(node, i) {
+                $.each(this.searchNodes, function(i, node) {
                     node.highlight = highlight;
                     _self.ztreeObj.updateNode(node);
                 })
@@ -100,10 +130,10 @@
             //显示隐藏搜索过滤
             searchFilterShow: function(event) {
                 var keyword = $.trim(event.target.value),
-                    allNodes = this.ztreeObj.getNodes(), //所有节点
-                    searchNodes = this.ztreeObj.getNodesByParamFuzzy('name', keyword);; //搜索匹配节点
+                    allNodes = this.ztreeObj.getNodes(); //所有节点
+                this.searchNodes = this.ztreeObj.getNodesByParamFuzzy('name', keyword);; //搜索匹配节点
                 this.ztreeObj.hideNodes(allNodes);
-                searchNodes.forEach(function(node, i) {
+                $.each(this.searchNodes, function(i, node) {
                     (function(searchNode) {
                         if (!searchNode) return false; //如果节点不存在，退出
                         var searchParentNode = searchNode.getParentNode(); //搜索匹配节点的父节点
